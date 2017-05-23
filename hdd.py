da962d3065025d8f02da05cbbc857f4a924c3480
hades <328705509@qq.com>
2017-05-23 17:15:11 +0800

    增加坐标计算模块

--- hdd.py
+++ hdd.py
@@ -18,7 +18,7 @@ def read_dict(fo):
 def init():
     global Grammar
     global GrammarObj
-    fo = open('d:\py\ProbabilityGrammar\\input.txt', 'r')
+    fo = open('input.txt', 'r')
     ##get structure imformation
     # get Roof
     Roof = read_dict(fo)
@@ -47,7 +47,10 @@ def init():
     Grammarnum = read_dict(fo)
     for i in range(int(Grammarnum['GrammarNum'])):
         temp = fo.readline()
-        Grammar.append(temp.split())
+        temp = temp.split()
+        Grammar.append(temp)
+        for j in xrange(2, len(temp) - 1, 2):
+            Grammar[i][j] = eval(Grammar[i][j])
         Grammar[i][-1] = float(Grammar[i][-1])
     fo.close()
 
@@ -60,7 +63,8 @@ def progra(root):
     for g in Grammar:
         if g[0] == root:
             temp = g[-1]
-            for i in range(1, len(g) - 1):
+            for i in range(1, len(g) - 1, 2):
+                # if not isinstance(g[i],str) : break
                 Name = g[i]
                 Proba = progra(Name)
                 temp *= Proba
@@ -71,28 +75,75 @@ def progra(root):
     return res
 
 
-def outp(root):
+def Sum(a, b):
+    return a + b
+
+
+def GetTruePosition(root, NowCoodinate):
+    father = GrammarObj[root]
+    if father in [1, 2, 4, 8]:
+        TruePosition[root] = NowCoodinate
+        return
+    g = BestRule[root]
+    for i in range(1, len(g) - 1, 2):
+        NewPosition = map(Sum, NowCoodinate, g[i + 1])
+        GetTruePosition(g[i], NewPosition)
+
+
+def PrintCombination(root):
     father = GrammarObj[root]
     if father in [1, 2, 4, 8]:
         return
     g = BestRule[root]
-    fw.write('%s -> %s' % (root, g[1]))
-    for i in range(2, len(g) - 1):
-        fw.write(' || %s' % g[i])
+    fw.write('%s -> %s' % (root.center(CENTERWIDTH), g[1].center(CENTERWIDTH)))
+    for i in range(3, len(g) - 1, 2):
+        fw.write(' || %s' % g[i].center(CENTERWIDTH))
     fw.write('\n')
-    for i in range(1, len(g) - 1):
-        outp(g[i])
+    for i in range(1, len(g) - 1, 2):
+        PrintCombination(g[i])
+
+
+class Vector3():
+    def __init__(self, x, y, z):
+        self.x, self.y, self.z = x, y, z
+
+    def fprint(self, fo):
+        fo.write('(%10f,%10f,%10f)' % (self.x, self.y, self.z))
+
+    def sprint(self):
+        print '(%f,%f,%f)' % (self.x, self.y, self.z)
+
+
+def PrintTruePosition(TruePosition):
+    fw.write('\nPosition of Each Object:\n')
+    for i in TruePosition:
+        Vec = Vector3(TruePosition[i][0], TruePosition[i][1], TruePosition[i][2])
+        fw.write('%s:' % i.center(CENTERWIDTH))
+        Vec.fprint(fw)
+        fw.write('\n')
+
 
 import fileinput
 import re
+import random
+import os
 
+os.chdir('d:\\py\\ProbabilityGrammar')
+TruePosition = {}
 GrammarObj = {}
 Grammar = []
 BestRule = {}
+root = 'Building'
+InputFile = 'inout.txt'
+OutputFile = 'output.txt'
+CENTERWIDTH = 15
 init()
-res = progra('Building')
-fw = open('d:\py\ProbabilityGrammar\\output.txt', 'w')
+res = progra(root)
+GetTruePosition(root, [29.409536, 39.305736, 44.277932, 0, 0, 0])
+fw = open(OutputFile, 'w')
 fw.write('The best answer of P = %f\n' % res)
-outp('Building')
+fw.write('\nGrammar of Each Object:\n')
+PrintCombination(root)
+PrintTruePosition(TruePosition)
 fw.close()
 ## open file "d:\py\ProbabilityGrammar\\output.txt"
